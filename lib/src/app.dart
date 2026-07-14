419b69f388e6eb94a9c369e8d4883e1a687ec91b
jtahun <jtahun@bk.ru>
2026-07-14 17:12:29 +0500

    some changes One

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tms_quiz/src/core/routing/app_router.dart';
 
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp.router(
+      routerConfig: appRouter.config(),
+    );
   }
 }
